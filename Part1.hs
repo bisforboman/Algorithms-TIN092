56b20b4a4950c2793eea202539e901ced8dd1105
Jakob Boman <bisforboman@gmail.com>
2011-09-05 18:06:38 +0200

    Added code logic

--- Part1.hs
+++ Part1.hs
@@ -1,5 +1,31 @@
 module Part1 where
 
-type Path = [Int] -- e.g. [x,y,z] ==> v1 = x, v2 = y, v3 = z
-type Graph = (Int, [[Int]], [Double]) -- (num_nodes, cost_matrix, probabilities)
+import Data.List
 
+type Path = [Int] -- e.g. [x,y,z] ==> v1 = x, v2 = y, v3 = z
+type Graph = ([[Int]], [Double]) -- (cost_matrix, probabilities)
+
+-- | Given number of nodes, generate all possible paths
+generatePaths :: Int -> [Path]
+generatePaths n = map (0:) $ permutations [1..n-1]
+
+pathCost :: Graph -> Path -> Double
+pathCost (edgeMatrix, costs) path = 
+    sum $ zipWith aux accumulativeEdgeCosts path
+  where
+    edges :: [(Int, Int)]
+    edges = path `zip` tail path
+    edgeCosts :: [Int]
+    edgeCosts = map (\(from,to) -> edgeMatrix !! from !! to) edges
+    accumulativeEdgeCosts :: [Int]
+    accumulativeEdgeCosts = scanl (+) 0 edgeCosts
+    aux :: Int -> Int -> Double
+    aux acc ix = fromIntegral acc * (costs !! ix)
+    
+
+-- The master function
+bestPathAndCost :: Graph -> (Double, Path)
+bestPathAndCost g@(edgeMatrix, costs) = minimum latenciesAndPaths
+  where
+    latenciesAndPaths = map (pathCost g) paths `zip` paths
+    paths = generatePaths (length edgeMatrix)
